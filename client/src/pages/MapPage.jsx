786a813eb5419eb4e7b22fef0ef8dea31537e1ce
Test <test@example.com>
2026-09-15 15:48:03 +0530

    use OSM tiles with CSS dark filter, no API key needed

--- client/src/pages/MapPage.jsx
+++ client/src/pages/MapPage.jsx
@@ -479,10 +479,9 @@ export default function MapPage() {
 
         <MapContainer center={CENTER} zoom={16} style={{ height: '100%', width: '100%' }} zoomControl={false}>
           <TileLayer
-            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-            subdomains="abcd"
-            maxZoom={20}
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            className="map-tiles-dark"
           />
           <ZoomControl position="bottomright" />
           <MapController flyTo={flyTo} fitBounds={fitBounds} />
